3ee27e11459b0857d3c5735ff34644f78b4a95d3
Jorge Campos <jorge.campos@medianet.es>
2015-09-30 09:17:17 +0200

    Added functionality currency converter to pass tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,10 @@
+var objExchangeRate = {
+  "EUR": {
+    "USD": 1.123,
+    "INR": 73.9965
+  }
+};
+
 function calc(num1, num2, op) {
   switch(op) {
     case "+":
@@ -13,5 +20,18 @@ function calc(num1, num2, op) {
       break;
     default:
       return NaN
+  }
+}
+
+function currencyConverter(num, fromCurrency, toCurrency){
+  for (var iFromCurrency in objExchangeRate) {
+    var defaultExchangeRate = objExchangeRate[iFromCurrency]
+    if(iFromCurrency == fromCurrency) {
+      for (var iToCurrency in defaultExchangeRate) { 
+        if(iToCurrency == toCurrency) {
+          return Number(calc(num, defaultExchangeRate[iToCurrency], '*').toFixed(2));
+        }
+      }
+    }
   }
 }

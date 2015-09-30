8180b5643230c713008bfcca05dde4b50877d48a
Jorge Campos <jorge.campos@medianet.es>
2015-09-30 09:11:49 +0200

    Added test for a simple calculator

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,18 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(true).toBe(true);
-      });
+  describe('Calculator', function() {
+    it('can add positive integers', function() {
+      expect(calc(3, 1, '+')).toEqual(4);  
+    });
+    it("can subtract positive integers", function() {
+      expect(calc(8, 5, '-')).toEqual(3);
+    });
+    it("can multiply positive integers", function() {
+      expect(calc(4, 3, '*')).toEqual(12);
+    });
+    it("can divide positive integers", function() {
+      expect(calc(12, 4, '/')).toEqual(3);
     });
   });
 })();

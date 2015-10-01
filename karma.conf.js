5a03d62cb7c5f7dd7bc343d772498c250180fcd6
Jorge Campos <jorge.campos@medianet.es>
2015-10-01 17:39:43 +0200

    Fixed bug in karma configuration file

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/*.js': ['coverage'], 'src/*.js': ['jshint']
+      'src/*.js': ['coverage', 'jshint']
     },
 
 
@@ -36,14 +36,6 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
-
-    // optionally, configure the reporter
-    coverageReporter: {
-      type : 'text',
-      dir : 'coverage/',
-      file : 'coverage.txt'
-    },
-
 
     // web server port
     port: 9876,

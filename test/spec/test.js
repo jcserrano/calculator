06516381e2d3adfee4ecacdf8ff886347154f45e
Jorge Campos <jorge.campos@medianet.es>
2015-10-06 17:17:22 +0200

    Updated tests with modules

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,53 +1,50 @@
 (function () {
   'use strict';
 
-  describe('A simple calculator', function() {
-    var calc;
-    beforeEach(function() {
-      calc = new Calculator();
-    });
+  var converter = require('../../src/converter.js');
+  var calculator = require('../../src/calculator.js');
 
+  describe('A simple calculator', function() {
     it('can add positive integers', function() {
-      expect(calc.add(3,1)).toEqual(4);
+      expect(calculator.add(3,1)).toEqual(4);
     });
 
     it("can subtract positive integers", function() {
-      expect(calc.sub(8,5)).toEqual(3);
+      expect(calculator.sub(8,5)).toEqual(3);
     });
 
     it("can multiply positive integers", function() {
-      expect(calc.mult(4,3)).toEqual(12);
+      expect(calculator.mult(4,3)).toEqual(12);
     });
 
     it("can divide positive integers", function() {
-      expect(calc.div(12,4)).toEqual(3);
+      expect(calculator.div(12,4)).toEqual(3);
     });
   });
 
   describe('A calculator with currency converter', function() {
     var fakeConverter;
-    var calcWithFkConv;
 
     beforeEach(function() {
-      fakeConverter = jasmine.createSpyObj('CurrencyConverter', ['calc', 'getExchangeRate']);
-      calcWithFkConv = new Calculator(fakeConverter);
+      fakeConverter = jasmine.createSpyObj('CurrencyConverter', ['calc']);
+      calculator.addConverter(fakeConverter);
     });
 
     it("A spy converter calls the calc function", function() {
-      calcWithFkConv.currencyConverter(50, "EUR", "INR");
+      calculator.currencyConverter(50, "EUR", "INR");
 
       expect(fakeConverter.calc).toHaveBeenCalled();
     });
 
     it("A spy converter calls one time the calc function", function() {
-      calcWithFkConv.currencyConverter(10, "EUR", "USD");
+      calculator.currencyConverter(10, "EUR", "USD");
 
       expect(fakeConverter.calc.calls.count()).toEqual(1);
     });
 
     it("A spy converter tracks all the arguments in the calc function", function() {
-      calcWithFkConv.currencyConverter(10, "EUR", "USD");
-      calcWithFkConv.currencyConverter(50, "EUR", "INR");
+      calculator.currencyConverter(10, "EUR", "USD");
+      calculator.currencyConverter(50, "EUR", "INR");
 
       expect(fakeConverter.calc).toHaveBeenCalledWith(10, "EUR", "USD");
       expect(fakeConverter.calc).toHaveBeenCalledWith(50, "EUR", "INR");
@@ -58,8 +55,8 @@
         return 0;
       });
 
-      var result = calcWithFkConv.currencyConverter(50, "EUR", "INR");
-      var result1 = calcWithFkConv.currencyConverter(10, "USD", "INR");
+      var result = calculator.currencyConverter(50, "EUR", "INR");
+      var result1 = calculator.currencyConverter(10, "USD", "INR");
 
       expect(result).toEqual(0);
       expect(result1).toEqual(0);
@@ -67,11 +64,6 @@
   });
 
   describe('A currency converter', function() {
-    var converter;
-    beforeEach(function() {
-      converter = new CurrencyConverter();
-    });
-
     it('should can convert Euros (EUR) to US Dollars (USD)', function() {
       var result = converter.calc(10, "EUR", "USD");
 
@@ -82,24 +74,6 @@
       var result = converter.calc(50, "EUR", "INR");
 
       expect(result).toEqual(3699.82);
-    });
-
-    it('should get exchange rate with  correct currency', function() {
-      var rate = converter.getExchangeRate("EUR", "INR");
-
-      expect(rate).toEqual(73.9965);
-    });
-
-    it('should not get exchange rate  with incorrect fromCurrency', function() {
-      var rate = converter.getExchangeRate("FRA", "INR");
-
-      expect(rate).toBeUndefined();
-    });
-
-    it('should not get exchange rate with incorrect toCurrency', function() {
-      var rate = converter.getExchangeRate("EUR", "FRA");
-
-      expect(rate).toBeUndefined();
     });
   });
 })();

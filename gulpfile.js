15d846e624be2b6a6d4a653cb5203a54d937d41a
Jorge Campos <jorge.campos@medianet.es>
2015-10-16 12:05:34 +0200

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,65 @@
-var gulp = require('gulp');
-var plato = require('gulp-plato');
+'use strict';
 
-var Server = require('karma').Server;
+var gulp   = require('gulp');
+var plugins = require('gulp-load-plugins')();
 
-gulp.task('test', function (done) {
-  return new Server({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done).start();
+var testFiles = ['./test/**/*.js', '!test/{temp,fixtures,temp/**,fixtures/**}'];
+var paths = {
+  lint: ['./gulpfile.js', './src/**/*.js'],
+  watch: ['./gulpfile.js', './src/**'].concat(testFiles),
+  tests: testFiles,
+  source: ['./src/**/*.js']
+};
+
+var plumberConf = {};
+
+if (process.env.CI) {
+  plumberConf.errorHandler = function(err) {
+    throw err;
+  };
+}
+
+gulp.task('lint', function () {
+  return gulp.src(paths.lint)
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.plumber(plumberConf))
+    .pipe(plugins.jscs())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('plato', function () {
-  return gulp.src(__dirname + '/src/*.js')
-    .pipe(plato('reports/plato', {
-        jshint: {
-            options: {
-                camelcase: true,
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                boss: true,
-                devel: true,
-                eqnull: true,
-                browser: true
-            }
-        },
-        complexity: {
-            trycatch: true
-        }
-    }));
+gulp.task('istanbul', function (cb) {
+  gulp.src(paths.source)
+    .pipe(plugins.istanbul()) // Covering files
+    .pipe(plugins.istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', function () {
+      gulp.src(paths.tests)
+        .pipe(plugins.plumber(plumberConf))
+        .pipe(plugins.mocha())
+        .on('error', process.exit.bind(process, 1)) //if mocha test fail exit with code 1
+        .pipe(plugins.istanbul.writeReports())  // Creating the reports after tests runned
+        .pipe(plugins.istanbul.writeReports({reporters: ['cobertura']}))
+        .on('finish', function() {
+          process.chdir(__dirname);
+          cb();
+        });
+    });
 });
+
+gulp.task('watch', ['test'], function () {
+  gulp.watch(paths.watch, function() {
+    // use child process, otherwise will get error.
+    require('child_process').spawn('gulp', ['test'], {
+      env: process.env,
+      cwd: process.cwd(),
+      stdio: [
+        process.stdin,
+        process.stdout,
+        process.stderr
+      ]
+    });
+  });
+});
+
+gulp.task('test', ['lint', 'istanbul']);
+
+gulp.task('default', ['watch']);
